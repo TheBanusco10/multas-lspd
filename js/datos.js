a9e715e82999bf6b9cd406b6a5b084daca82481d
TheBanusco10 <thebanusco10@gmail.com>
2020-01-19 15:34:38 +0100

    Multas actualizadas

--- js/datos.js
+++ js/datos.js
@@ -37,144 +37,175 @@ var multas = {
             "Federal": "0"
         },
         {
-            "Nombre": "2.7 Parada peligrosa y/o no permitida",
+            "Nombre": "2.7 Parada peligrosa",
             "Cantidad": 0,
             "Precio": "500",
             "Federal": "0"
         },
         {
-            "Nombre": "2.8 Saltarse un Stop",
+            "Nombre": "2.8 Circular marcha atrás",
+            "Cantidad": 0,
+            "Precio": "600",
+            "Federal": "0"
+        },
+        {
+            "Nombre": "2.9 Saltarse un Stop",
             "Cantidad": 0,
             "Precio": "650",
             "Federal": "0"
         },
         {
-            "Nombre": "2.9 Adelantamiento peligroso",
+            "Nombre": "2.10 Adelantamiento peligroso",
             "Cantidad": 0,
             "Precio": "800",
             "Federal": "0"
         },
         {
-            "Nombre": "2.10 Obstruir el tráfico",
+            "Nombre": "2.11 Obstruir el tráfico",
             "Cantidad": 0,
             "Precio": "900",
             "Federal": "0"
         },
         {
-            "Nombre": "2.11 Saltarse un semáforo en rojo",
+            "Nombre": "2.12 Saltarse un semáforo en rojo",
             "Cantidad": 0,
             "Precio": "900",
             "Federal": "0"
         },
         {
-            "Nombre": "2.12 No llevar protección personal",
+            "Nombre": "2.13 No ceder el paso a los vehículos de emergencia",
+            "Cantidad": 0,
+            "Precio": "900",
+            "Federal": "0"
+        },
+        {
+            "Nombre": "2.14 No llevar protección personal",
             "Cantidad": 0,
             "Precio": "1200",
             "Federal": "0"
         },
         {
-            "Nombre": "2.13 Circular en dirección contraria",
+            "Nombre": "2.15 Circular en dirección contraria",
             "Cantidad": 0,
             "Precio": "1500",
             "Federal": "0"
         },
         {
-            "Nombre": "2.14 Exceso de velocidad +10 km/h",
+            "Nombre": "2.16 Exceso de velocidad +10 km/h",
             "Cantidad": 0,
             "Precio": "500",
             "Federal": "0"
         },
         {
-            "Nombre": "2.15 Exceso de velocidad +11/20 km/h",
+            "Nombre": "2.17 Exceso de velocidad +11/20 km/h",
             "Cantidad": 0,
             "Precio": "750",
             "Federal": "0"
         },
         {
-            "Nombre": "2.16 Exceso de velocidad +21/40 km/h",
+            "Nombre": "2.18 Exceso de velocidad +21/40 km/h",
             "Cantidad": 0,
             "Precio": "1050",
             "Federal": "0"
         },
         {
-            "Nombre": "2.17 Exceso de velocidad +41/60 km/h",
+            "Nombre": "2.19 Exceso de velocidad +41/60 km/h",
             "Cantidad": 0,
             "Precio": "1350",
             "Federal": "0"
         },
         {
-            "Nombre": "2.18 Exceso de velocidad +61 km/h",
+            "Nombre": "2.20 Exceso de velocidad +61 km/h",
             "Cantidad": 0,
             "Precio": "1550",
             "Federal": "0"
         },
         {
-            "Nombre": "2.18 Ocasionar un accidente de tráfico",
+            "Nombre": "2.21 Ocasionar un accidente de tráfico",
             "Cantidad": 0,
             "Precio": "1500",
             "Federal": "0"
         },
         {
-            "Nombre": "2.20 Circular sin permiso de conducir",
+            "Nombre": "2.22 Conducción temeraria",
+            "Cantidad": 0,
+            "Precio": "2500",
+            "Federal": "0"
+        },
+        {
+            "Nombre": "2.23 Circular sin permiso de conducir",
             "Cantidad": 0,
             "Precio": "3800",
             "Federal": "0"
         },
     ],
-    "Leves": [{
-            "Nombre": "3.1 Insultar, gritar y/o faltar el respeto a un civil",
+    "Leves": [
+        {
+            "Nombre": "3.1 No portar el DNI",
+            "Cantidad": 0,
+            "Precio": "400",
+            "Federal": "0"
+        },
+        {
+            "Nombre": "3.2 Rostro oculto",
+            "Cantidad": 0,
+            "Precio": "600",
+            "Federal": "0"
+        },
+        {
+            "Nombre": "3.3 Insultar, gritar y/o faltar el respeto a un civil",
             "Cantidad": 0,
             "Precio": "900",
             "Federal": "0"
         },
         {
-            "Nombre": "3.2 Manifestación ilegal",
+            "Nombre": "3.4 Manifestación ilegal",
             "Cantidad": 0,
             "Precio": "1100",
             "Federal": "0"
         },
         {
-            "Nombre": "3.3 Insultar, gritar y/o faltar el respeto a un agente / EMS",
+            "Nombre": "3.5 Insultar, gritar y/o faltar el respeto a un agente / EMS",
             "Cantidad": 0,
             "Precio": "1700",
             "Federal": "0"
         },
 
         {
-            "Nombre": "3.4 Alteración del orden público",
+            "Nombre": "3.6 Alteración del orden público",
             "Cantidad": 0,
             "Precio": "2100",
             "Federal": "0"
         },
         {
-            "Nombre": "3.5 Amenaza verbal o intimidación a un civil",
+            "Nombre": "3.7 Amenaza verbal o intimidación a un civil",
             "Cantidad": 0,
             "Precio": "3700",
             "Federal": "0"
         },
         {
-            "Nombre": "3.6 Intento de fuga",
+            "Nombre": "3.8 Intento de fuga",
             "Cantidad": 0,
             "Precio": "4500",
             "Federal": "5"
         },
         {
-            "Nombre": "3.7 Amenaza verbal o intimidación a los agentes / EMS",
+            "Nombre": "3.9 Ocasionar problemas a la LSPD / EMS",
+            "Cantidad": 0,
+            "Precio": "5000",
+            "Federal": "7"
+        },
+        {
+            "Nombre": "3.10 Amenaza verbal o intimidación a un agente / EMS",
             "Cantidad": 0,
             "Precio": "5500",
             "Federal": "5"
-        },
-        {
-            "Nombre": "3.8 Ocasionar problemas a la LSPD / EMS",
-            "Cantidad": 0,
-            "Precio": "6000",
-            "Federal": "7"
         }
     ],
     "Medias": [{
             "Nombre": "4.1 Portar arma sin licencia",
             "Cantidad": 0,
-            "Precio": "4200",
+            "Precio": "3200",
             "Federal": "0"
         },
         {
@@ -214,7 +245,7 @@ var multas = {
             "Federal": "5"
         },
         {
-            "Nombre": "4.8 Tenencia ilegal de marihuana (+5UD)",
+            "Nombre": "4.8 Tenencia ilegal de estupefacientes (+5UD)",
             "Cantidad": 0,
             "Precio": "8000",
             "Federal": "10"
@@ -241,7 +272,7 @@ var multas = {
             "Nombre": "4.12 Tentar a la corrupción a un agente / EMS",
             "Cantidad": 0,
             "Precio": "9000",
-            "Federal": "20"
+            "Federal": "15"
         },
         {
             "Nombre": "4.13 Posesión de arma ilegal (Ligera)",
@@ -251,82 +282,82 @@ var multas = {
         }
     ],
     "Graves": [{
-            "Nombre": "5.1 Fabricación o recolección de marihuana",
+            "Nombre": "5.1 Fabricación o recolección de estupefacientes",
             "Cantidad": 0,
-            "Precio": "9000",
+            "Precio": "6000",
+            "Federal": "10"
+        },
+        {
+            "Nombre": "5.2 Asesinato involuntario de un civil (Sin mente)",
+            "Cantidad": 0,
+            "Precio": "7000",
             "Federal": "12"
         },
         {
-            "Nombre": "5.2 Asesinato involuntario de un civil",
+            "Nombre": "5.3 Venta ilegal de estupefacientes",
             "Cantidad": 0,
-            "Precio": "8000",
-            "Federal": "12"
+            "Precio": "10000",
+            "Federal": "10"
         },
         {
-            "Nombre": "5.3 Robo de tienda",
+            "Nombre": "5.4 Robo de tienda",
             "Cantidad": 0,
             "Precio": "12000",
             "Federal": "8"
         },
         {
-            "Nombre": "5.4 Secuestro de un rehén",
+            "Nombre": "5.5 Secuestro de un rehén",
             "Cantidad": 0,
             "Precio": "13000",
             "Federal": "16"
         },
         {
-            "Nombre": "5.5 Posesión de arma ilegal (Media)",
+            "Nombre": "5.6 Posesión de arma ilegal (Media)",
             "Cantidad": 0,
             "Precio": "15000",
             "Federal": "17"
         },
         {
-            "Nombre": "5.6 Robo de Joyería",
+            "Nombre": "5.7 Robo de Joyería",
             "Cantidad": 0,
             "Precio": "15000",
             "Federal": "15"
         },
         {
-            "Nombre": "5.7 Intento de homicidio a un civil",
+            "Nombre": "5.8 Intento de homicidio a un civil",
             "Cantidad": 0,
             "Precio": "16000",
             "Federal": "16"
         },
         {
-            "Nombre": "5.8 Procesado de marihuana II (+100UD)",
+            "Nombre": "5.9 Procesado de estupefacientes II (+100UD)",
             "Cantidad": 0,
             "Precio": "16000",
             "Federal": "12"
         },
         {
-            "Nombre": "5.9 Intento de homicidio a un policía / EMS",
+            "Nombre": "5.10 Intento de homicidio a un policía / EMS",
             "Cantidad": 0,
             "Precio": "25000",
             "Federal": "18"
         },
         {
-            "Nombre": "5.10 Venta ilegal de marihuana",
+            "Nombre": "5.11 Venta de armas ilegal",
             "Cantidad": 0,
-            "Precio": "25000",
-            "Federal": "20"
-        },
-        {
-            "Nombre": "5.11 Venta de armas ilegales",
-            "Cantidad": 0,
-            "Precio": "25000",
+            "Precio": "27000",
             "Federal": "20"
         },
         {
             "Nombre": "5.12 Robo de banco",
             "Cantidad": 0,
-            "Precio": "25000",
-            "Federal": "30"
+            "Precio": "27000",
+            "Federal": "27"
         },
         {
-            "Nombre": "5.13 Asesinato de un civil",
+            "Nombre": "5.13 Intento / Asesinato de un civil",
             "Cantidad": 0,
-            "Precio": "27000",
-            "Federal": "30"
+            "Precio": "28000",
+            "Federal": "25"
         },
         {
             "Nombre": "5.14 Posesión de arma ilegal (Pesada)",
@@ -351,13 +382,13 @@ var multas = {
             "Nombre": "5.17 Robo de Banco federal",
             "Cantidad": 0,
             "Precio": "60000",
-            "Federal": "60"
+            "Federal": "70"
         },
         {
             "Nombre": "5.18 Asesinato de un Agente / EMS",
             "Cantidad": 0,
             "Precio": "60000",
-            "Federal": "55"
+            "Federal": "70"
         },
     ]
 }
